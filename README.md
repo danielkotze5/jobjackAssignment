93e466cfa1f11d36aa8984317657528f9b51435a
Daniel Kotze <daniel.kotze@mixtelematics.com>
2020-03-05 11:21:12 +0200

    File permissions display added.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 #Server instructions:
-#run nodemon index.js in die root directory. Die server is nog nie ge-dockerize nie.
+run nodemon index.js in die root directory. Die server is nog nie ge-dockerize nie. Server output = localhost port 5000.
 
 #Client instructions:
-#Run die docker image of run ng serve in die root directory.
+Run die docker image of run ng serve in die root directory. Client runs on localhost port 4200.
 
 #Notas:
-#Let wel ek moes die Moesif CORS google chrom extention install om CORS errors te vermy. Weet nie of hierdie vir julle sal help nie.
+Let wel ek moes die Moesif CORS google chrom extention install om CORS errors te vermy. Weet nie of hierdie vir julle sal help nie.
--- nodeServer/lib/fileManager.js
+++ nodeServer/lib/fileManager.js
@@ -4,14 +4,64 @@ var path = require('path');
 var FileManager = {};
 
 FileManager.getStats = function *(p) {
+
   var stats = yield fs.stat(p);
+  let permisString = '0' + (stats.mode & parseInt('777', 8)).toString(8);
+  let char0 = permisString.charAt(0);
+  let char1 = permisString.charAt(1);
+  let char2 = permisString.charAt(2);
+  let char3  = permisString.charAt(3);
+
+  let processedRaw = function(charIn){
+
+    let stringOut = "";
+
+    switch(charIn){      
+      case '0':{
+        stringOut = "none";
+        break;
+      }
+      case '1':{
+        stringOut = "execute";
+        break;
+      }
+      case '2':{
+        stringOut = "write";
+        break;
+      }
+      case '3':{
+        stringOut = "write,execute";
+        break;
+      }
+      case '4':{
+        stringOut = "read";
+        break;
+      }
+      case '5':{
+        stringOut = "read,execute";
+        break;
+      }
+      case '6':{
+        stringOut = "read,write";
+        break;
+      }
+      case '7':{
+        stringOut = "read,write,execute";
+        break;
+      }
+    }   
+    return stringOut;
+  }
 
   return {
     folder: stats.isDirectory(),
     size: stats.size,    
 	  birthdate: stats.birthtime.toLocaleDateString("en-US"),
-  	birthtime: stats.birthtime.toLocaleTimeString("en-US"),
-  	myPermissions : '0' + (stats.mode & parseInt('777', 8)).toString(8),
+  	birthtime: stats.birthtime.toLocaleTimeString("en-US"),        
+    permissionSpecial: processedRaw(char0),
+    permissionUser: processedRaw(char1),
+    permissionGroup: processedRaw(char2),
+    permissionOther: processedRaw(char3),    
   }  
 };
 
